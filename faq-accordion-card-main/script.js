11fe8a225a0fac6ee55e22b8fbaa2aafc49a0a0c
damlakara5 <78643920+damlakara5@users.noreply.github.com>
2021-09-21 11:34:16 +0300

    update

--- faq-accordion-card-main/script.js
+++ faq-accordion-card-main/script.js
@@ -1,14 +1,42 @@
 let button = document.querySelectorAll(".question");
+let arrow = document.querySelectorAll(".arrow");
+let answwerList = document.querySelectorAll(".answer");
 
 for (let i = 0; i < button.length; i++) {
-  button[i].addEventListener("click", function () {
+  button[i].addEventListener("click", function (event) {
+    let x = event.target;
     let answer = this.nextElementSibling;
-    answer.classList.contains("hidden")
-      ? answer.classList.remove("hidden")
-      : answer.classList.add("hidden");
+    for (let i = 0; i < answwerList.length; i++) {
+      answwerList[i] !== answer
+        ? answwerList[i].classList.add("hidden")
+        : console.log();
+    }
 
-    document.querySelector(".arrow").classList.contains("rotate")
-      ? document.querySelector(".arrow").classList.remove("rotate")
-      : document.querySelector(".arrow").classList.add("rotate");
+    answer.classList.toggle("hidden");
+    /*     answer.classList.contains("hidden")
+      ? answer.classList.remove("hidden")
+      : answer.classList.add("hidden"); */
+
+    arrow[i].classList.contains("rotate")
+      ? arrow[i].classList.remove("rotate")
+      : arrow[i].classList.add("rotate");
+
+    for (let j = 0; j < button.length; j++) {
+      if (button[j] === x) {
+        x.classList.contains("bold")
+          ? x.classList.remove("bold")
+          : x.classList.add("bold");
+      } else {
+        button[j].classList.remove("bold");
+      }
+    }
+
+    /*     if (answer.classList.contains("hidden")) {
+      button[i].style.color = "hsl(240, 6%, 50%)";
+      button[i].style.fontWeight = 300;
+    } else {
+      button[i].style.color = "black";
+      button[i].style.fontWeight = 700;
+    } */
   });
 }
